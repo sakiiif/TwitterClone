a5a5aa18d2b06b30923446139d541e764419562a
= <=>
2026-08-15 22:40:52 +0600

    created BaseEntity class & inherited by the all related class

---
+++ TwitterClone.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterClone.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; private set; }
+        public DateTime CreatedAt { get; }
+        public DateTime? ModifiedAt { get;  set; } // ? means Nullable data/property
+        public Guid CreatedBy { get; set; } // done by admins probably
+        public Guid? ModifiedBy { get; set; } // done by admins probably
+
+        public BaseEntity () // constructor
+        {
+            Id = Guid.NewGuid ();
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        
+    }
+}
--- TwitterClone.Domain/Entities/Comment.cs
+++ TwitterClone.Domain/Entities/Comment.cs
@@ -4,30 +4,23 @@ using System.Text;
 
 namespace TwitterClone.Domain.Entities
 {
-    public class Comment
+    public class Comment : BaseEntity
     {
-        private Guid _id;
         private Guid _userId;
         private Guid _tweetId;
-        private DateTime _createdAt;
-        private DateTime _modifiedAt; // not sure why this
-        private Guid _createdBy; // done by admins probably
-        private Guid _modifiedBy; // done by admins probably
+        private string _content;
 
-        public Comment(Guid userId, Guid tweetId) // constructor
+        public Comment(Guid userId, Guid tweetId, string msg) : base() // constructor
         {
-            _id = Guid.NewGuid();
-            _createdAt = DateTime.UtcNow;
             _userId = userId;
             _tweetId = tweetId;
+            _content = msg;
         }
 
         // properties
-        public Guid Id { get { return _id; } }
         public Guid TweetId { get { return _tweetId; } }
         public Guid UserId { get { return _userId; } }
-        public DateTime CreatedAt { get { return _createdAt; } }
-        public DateTime ModifiedAt { get { return _modifiedAt; } set { _modifiedAt = value; } }
+        public string Content { get { return _content; } set { _content = value; } }
 
     }
 }
